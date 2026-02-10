8e1f65b1c21761177e9bc880b8af788b9b7ef386
siddhes07 <siddheshnj07@gmail.com>
2026-02-10 22:58:12 +0530

    two sum useing hash table

---
+++ two_sum_.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> mp;  // value -> index
+
+    for (int i = 0; i < nums.size(); i++) {
+        int need = target - nums[i];
+
+        if (mp.find(need) != mp.end()) {
+            return {mp[need], i};
+        }
+
+        mp[nums[i]] = i;
+    }
+
+    return {};
+}
+
+int main() {
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+
+    vector<int> result = twoSum(nums, target);
+
+    if (!result.empty()) {
+        cout << "Indices: " << result[0] << " " << result[1] << endl;
+    } else {
+        cout << "No solution found" << endl;
+    }
+
+    return 0;
+}
