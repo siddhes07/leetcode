931d6d69158de38b998d04e666cfed836ec10fe0
siddhes07 <siddheshnj07@gmail.com>
2026-02-19 23:25:02 +0530

    logest prifix

---
+++ prifix.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.empty()) return "";
+        
+        string prefix = strs[0];
+        
+        for(int i = 1; i < strs.size(); i++) {
+            while(strs[i].find(prefix) != 0) {
+                prefix = prefix.substr(0, prefix.size() - 1);
+                
+                if(prefix.empty())
+                    return "";
+            }
+        }
+        return prefix;
+    }
+};
+
+int main() {
+    Solution obj;
+    
+    vector<string> strs = {"flower", "flow", "flight"};
+    
+    string result = obj.longestCommonPrefix(strs);
+    
+    cout << "Longest Common Prefix: " << result << endl;
+    
+    return 0;
+}
