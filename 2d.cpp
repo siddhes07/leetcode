e8fd6df5aa342502a8a3a36dae1497bb02a6d435
siddhes07 <siddheshnj07@gmail.com>
2026-04-16 22:24:07 +0530

    Implement 2D array input and output in C++

---
+++ 2d.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows, cols;
+
+    cout << "Enter number of rows: ";
+    cin >> rows;
+    cout << "Enter number of columns: ";
+    cin >> cols;
+
+    int arr[rows][cols];
+
+    // Input
+    cout << "Enter elements:\n";
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    // Output
+    cout << "2D Array is:\n";
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
