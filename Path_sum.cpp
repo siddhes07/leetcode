e7842682c7601f537de6673b03eae35d8d649c79
siddhes07 <siddheshnj07@gmail.com>
2026-06-04 22:59:53 +0530

    Create Path_sum.cpp

---
+++ Path_sum.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int x) {
+        val = x;
+        left = right = NULL;
+    }
+};
+
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if (root == NULL) return false;
+
+        if (root->left == NULL && root->right == NULL)
+            return targetSum == root->val;
+
+        return hasPathSum(root->left, targetSum - root->val) ||
+               hasPathSum(root->right, targetSum - root->val);
+    }
+};
+
+int main() {
+    TreeNode* root = new TreeNode(5);
+    root->left = new TreeNode(4);
+    root->right = new TreeNode(8);
+    root->left->left = new TreeNode(11);
+    root->left->left->left = new TreeNode(7);
+    root->left->left->right = new TreeNode(2);
+    root->right->left = new TreeNode(13);
+    root->right->right = new TreeNode(4);
+    root->right->right->right = new TreeNode(1);
+
+    int targetSum = 22;
+
+    Solution obj;
+    if (obj.hasPathSum(root, targetSum))
+        cout << "True";
+    else
+        cout << "False";
+
+    return 0;
+}
