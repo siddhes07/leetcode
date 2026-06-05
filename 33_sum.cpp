b0a69eb769443b6ca6980eddfb6215abe894afee
siddhes07 <siddheshnj07@gmail.com>
2026-06-05 23:02:01 +0530

    solve the 3 sum letcode problem in cpp

---
+++ 33_sum.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
+        sort(nums.begin(), nums.end());
+
+        int n = nums.size();
+
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    result.push_back({nums[i], nums[left], nums[right]});
+
+                    while (left < right && nums[left] == nums[left + 1])
+                        left++;
+                    while (left < right && nums[right] == nums[right - 1])
+                        right--;
+
+                    left++;
+                    right--;
+                }
+                else if (sum < 0) {
+                    left++;
+                }
+                else {
+                    right--;
+                }
+            }
+        }
+
+        return result;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    vector<int> nums = {-1, 0, 1, 2, -1, -4};
+
+    vector<vector<int>> ans = sol.threeSum(nums);
+
+    cout << "Triplets:\n";
+    for (auto& triplet : ans) {
+        cout << "[ ";
+        for (int num : triplet) {
+            cout << num << " ";
+        }
+        cout << "]\n";
+    }
+
+    return 0;
+}
