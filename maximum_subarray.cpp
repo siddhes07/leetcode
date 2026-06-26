253aa837c9c0efc61078009ad80f1ebeda9cdf9e
siddhes07 <siddheshnj07@gmail.com>
2026-06-26 23:06:55 +0530

    solver 53 problem

---
+++ maximum_subarray.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSum = nums[0];
+        int currentSum = nums[0];
+
+        for (int i = 1; i < nums.size(); i++) {
+            currentSum = max(nums[i], currentSum + nums[i]);
+            maxSum = max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+};
+
+int main() {
+    vector<int> nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+    Solution sol;
+    int result = sol.maxSubArray(nums);
+
+    cout << "Maximum Subarray Sum: " << result << endl;
+
+    return 0;
+}
