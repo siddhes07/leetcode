093d8caffefadb8b3cd084d9ed7b1b902fa15b3a
siddhes07 <siddheshnj07@gmail.com>
2026-06-28 20:40:40 +0530

    solve ther 83 problem of leetcode

---
+++ remove_duplicate.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    
+    ListNode(int x) {
+        val = x;
+        next = nullptr;
+    }
+};
+
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* curr = head;
+
+        while (curr != nullptr && curr->next != nullptr) {
+            if (curr->val == curr->next->val) {
+                curr->next = curr->next->next;
+            } else {
+                curr = curr->next;
+            }
+        }
+
+        return head;
+    }
+};
+
+void printList(ListNode* head) {
+    while (head) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(1);
+    head->next->next = new ListNode(2);
+    head->next->next->next = new ListNode(3);
+    head->next->next->next->next = new ListNode(3);
+
+    Solution sol;
+    head = sol.deleteDuplicates(head);
+
+    printList(head);
+
+    return 0;
+}
