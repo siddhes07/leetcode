ccd0d2b58586ab00339b13c5492e757f11f8d5cf
siddhes07 <siddheshnj07@gmail.com>
2026-07-20 23:11:35 +0530

    solve leetcode problem 152

---
+++ max_product.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int maxProd = nums[0];
+        int minProd = nums[0];
+        int result = nums[0];
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] < 0)
+                swap(maxProd, minProd);
+
+            maxProd = max(nums[i], maxProd * nums[i]);
+            minProd = min(nums[i], minProd * nums[i]);
+
+            result = max(result, maxProd);
+        }
+
+        return result;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    vector<int> nums = {2, 3, -2, 4};
+
+    cout << "Maximum Product Subarray = "
+         << sol.maxProduct(nums) << endl;
+
+    return 0;
+}
